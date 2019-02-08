6ab3be13fae76586879f16258842c9f16a72439b
Adam Smith <adamsmith910@aol.com>
2019-02-08 16:35:57 -0500

    provisional cart, navbar and products

---
+++ src/components/Products.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1 class="title">All Items</h1>
+      <p>{{length}} products</p>
+      <table class="table is-striped">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="product in products" track-by="id">
+            <td>{{product.name}}</td>
+            <td>${{product.price}}</td>
+            <td><button @click='addToCart(product)' class='button is-info'>Add to cart</button></td>
+          </tr>
+        </tbody>
+    </table>
+  </div>
+</template>
+<script>
+import { mapGetters, mapActions } from 'vuex'
+export default {
+  name: 'app',
+  computed: mapGetters({
+    products: 'allProducts',
+    length: 'getNumberOfProducts'
+  }),
+  methods: mapActions([
+    'addToCart'
+  ])
+}
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -9,23 +9,29 @@ const state = {
   added: [],
   all: [
     {
-      id: 'cc919e21-ae5b-5e1f-d023-c40ee669520c',
-      name: 'COBOL 101 vintage',
-      description: 'Learn COBOL with this vintage programming book',
-      price: 399
+      id: '1',
+      name: 'Vanilla',
+      price: 1
     },
     {
-      id: 'bcd755a6-9a19-94e1-0a5d-426c0303454f',
-      name: 'Sharp C2719 curved TV',
-      description: 'Watch TV like never before with the brand new curved screen technology',
-      price: 1995
+      id: '2',
+      name: 'Chocolate',
+      price: 1
     },
     {
-      id: '727026b7-7f2f-c5a0-ace9-cc227e686b8e',
-      name: 'Remmington X mechanical keyboard',
-      description: 'Excellent for gaming and typing, this Remmington X keyboard ' +
-        'features tactile, clicky switches for speed and accuracy',
-      price: 595
+      id: '3',
+      name:  'Mint Chocolate Chip'
+      price: 1
+    },
+    {
+      id: '4',
+      name:  'Sprinkles',
+      price: 1
+    },
+    {
+      id: '5',
+      name:  'Chocolate Chips',
+      price: 1
     }
   ]
 }
