58cc1ff6471145c3b7e015ef1c443a8430f26097
Adam Smith <adamsmith910@aol.com>
2019-02-10 14:19:54 -0500

    added menu to cart page

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -25,9 +25,32 @@
             <td><b>${{ total }}</b></td>
           </tr>
       </tbody>
-</table>
+    </table>
     <p><button v-show="products.length" class='button is-primary' @click='checkout(products)'>Checkout</button></p>
+
+    <div>
+      <h1 class="title">All Items</h1>
+        <table class="table is-striped">
+          <thead>
+            <tr>
+              <th>Quantity</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="product in products" :key="product.id">
+              <td>{{ product.measurement }}</td>
+              <td>{{ product.name }}</td>
+              <td>${{ product.price }}</td>
+              <td><button @click='addToCart(product)' class='button is-info'>Add to cart</button></td>
+            </tr>
+          </tbody>
+      </table>
+    </div>
   </div>
+
 </template>
 
 <script>
@@ -44,6 +67,7 @@
       }
     },
     methods: mapActions([
+                'addToCart',
                 'checkout'
     ])
   }
